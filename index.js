ae2f453244104322d6377cd11986d6a241b7956d
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-17 20:42:46 +0100

    setup prom-client

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const client =  require('prom-client');
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
 const getDate = require('./date/date');
+
 const app = express()
 app.use(express.json());
 
-app.use('/',  async (req, res) => {
-    res.send(getDate());
+app.get('/__/metrics', async (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    const metrics = await client.register.metrics();
+    // console.log(metrics);
+    return res.send(metrics)
 });
+
+app.get('/',  async (req, res) =>  res.send(getDate()));
 
 app.listen(3000, () => {
     console.log(`Example app listening on port 3000`)
